e2477a92664f699a7a344375a0455d2fa170e209
Ho3yn.Golchin <ho3yngolchin@gmail.com>
2022-07-27 17:41:09 +0430

    createLessons created !

--- src/lessons/lessons.controller.ts
+++ src/lessons/lessons.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
+import { CreateLessonDto } from './dto/create-lesson.dto';
+import { Lesson } from './lesson.entity';
+import { LessonsService } from './lessons.service';
 
 @Controller('lessons')
-export class LessonsController {}
+export class LessonsController {
+  private logger = new Logger('LessonsController');
+  constructor(private lessonsService: LessonsService) {}
+
+  @Post()
+  createLesson(@Body() createLessonDto: CreateLessonDto): Promise<Lesson> {
+    return this.lessonsService.createLesson(createLessonDto);
+  }
+}
